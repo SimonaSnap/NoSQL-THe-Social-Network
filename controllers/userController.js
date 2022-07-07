4259b39b8e5b36a712ecbece69c843473b665f1c
Simona <simona.snapk@gmail.com>
2022-07-06 23:42:46 -0700

    all user routes/controllers made, friend routes left reaction routes left

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,31 @@ const { User, Thought } = require("../models");
 
 module.exports = {
 
+    deleteUser(req, res)
+    {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) => !user
+                ? res.status(400).json({ message: "No User at this id" })
+                : res.json({ message: "GoodBye User" })
+            ).catch((err) =>
+                res.status(500).json(err))
+    },
+
+    updateUser(req, res)
+    {
+        User.findOneAndUpdate({ _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true })
+            .then((user) => !user
+                ? res.status(400).json({ message: "No User at this Id" })
+                : res.json(user)
+            ).catch((err) =>
+            {
+                console.log(err)
+                return res.status(500).json(err)
+            })
+    },
+
     findAllUsers(req, res)
     {
         User.find()
@@ -9,5 +34,20 @@ module.exports = {
             .catch((err) => res.status(500).json(err))
     },
 
+    findUserById(req, res)
+    {
+        User.findOne({ _id: req.params.userId })
+            .then(async (user) => !user
+                ? res.status(400).json({ message: "No User at this id" })
+                : res.json(user)
+            ).catch((err) => res.status(500).json(err));
+    },
 
+    createUser(req, res)
+    {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) =>
+                res.status(500).json(err));
+    }
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,19 +1,18 @@
 const router = require("express").Router();
 
 const {
-    // deleteUser,
-    // updateUser,
+    deleteUser,
+    updateUser,
     findAllUsers,
-    // findUserById,
-    // createUser,
+    findUserById,
+    createUser,
     // removeFriend,
     // addFriend
 } = require("../../controllers/userController");
 
-router.route("/").get(findAllUsers)
-// .post(createUser));
+router.route("/").get(findAllUsers).post(createUser);
 
-// router.route("/:userId").get(findUserById).put(updateUser).delete(deleteUser);
+router.route("/:userId").get(findUserById).put(updateUser).delete(deleteUser);
 
 // router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
